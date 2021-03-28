16fb88754226eba6033a3519862a53ad2acac3c0
acted <l.oad@qq.com>
2021-03-28 20:24:48 +0800

    初始化

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(int argc, char const *argv[])
+{
+    int a=0;
+    short b=0;
+    long c=1L;
+    float d=1.0F;
+    double e=1.0;
+    char character='dev';
+    printf("%d\n",sizeof(character));
+    return 0;
+}
+    
---
+++ test.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+#include<iostream>
+int main(int argc, char const *argv[])
+{
+    char a[255]="\0";
+    cin>>a;
+    cout<<a<<endl;
+    return 0;
+}
