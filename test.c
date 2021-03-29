23876792c7571e156c4e97633a9144600f7b4da7
acted <l.oad@qq.com>
2021-03-29 14:41:42 +0800

    字符串长度

--- test.c
+++ test.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-#define MAX 10
-enum Sex{
-    MALE,FEMALE,SECRET
-};
+#include <string.h>
+
 int main(int argc, char const *argv[])
 {
-    enum Sex s=FEMALE;
-    printf("%d",s);
+    char arr[]="abc";
+    char arr2[]={'a','b','c','\0'};
+    printf("内容:%s\n",arr);
+    printf("内容:%s\n",arr2);
+    printf("长度:%d\n",strlen(arr));
+    printf("长度:%d\n",strlen(arr2));
+    printf("大小:%d\n",sizeof arr);
+    printf("大小:%d\n",sizeof arr2);
     return 0;
 }
