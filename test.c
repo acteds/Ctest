16979a02cc9d1537039fc19f5cfb25a97f0d0fa5
acted <l.oad@qq.com>
2021-03-29 13:01:39 +0800

    scanf

--- test.c
+++ test.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <limits.h>
-int a=0;
+
 int main(int argc, char const *argv[])
 {
-    int a=1;
-    printf("%d\n",a);
+    int num1=0,num2=0;
+    scanf("%d %d",&num1,&num2);
+    printf("%d %d",num1,num2);
     return 0;
-
 }
