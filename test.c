3dc6aafd0ad49e5dd9a00ca3ff6d7fe6b0f910d4
acted <l.oad@qq.com>
2021-04-01 18:30:00 +0800

    update

--- test.c
+++ test.c
@@ -3,13 +3,8 @@
 
 int main(int argc, char const *argv[])
 {
-    char arr[]="abc";
-    char arr2[]={'a','b','c','\0'};
-    printf("内容:%s\n",arr);
-    printf("内容:%s\n",arr2);
-    printf("长度:%d\n",strlen(arr));
-    printf("长度:%d\n",strlen(arr2));
-    printf("大小:%d\n",sizeof arr);
-    printf("大小:%d\n",sizeof arr2);
+    char ch='w';
+    char*pc=&ch;
+    printf("%d",sizeof(pc));
     return 0;
 }
