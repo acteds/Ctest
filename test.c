85f7fd5d5a86271a8b0cd2ef9c7696effcd5e755
acted <l.oad@qq.com>
2021-04-01 20:00:29 +0800

    结构体

--- test.c
+++ test.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    char name[20];
+    short price;
+}person;
+
 int main(int argc, char const *argv[])
 {
-    char ch='w';
-    char*pc=&ch;
-    printf("%d",sizeof(pc));
+    person p={"Êé1",20};
+    person *p2=&p;
+    printf("name=%s,price=%d\n", p.name,p.price);
+    printf("name=%s,price=%d\n", (*p2).name,(*p2).price);
+    printf("name=%s,price=%d\n", p2->name,p2->price);
     return 0;
 }
