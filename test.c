ec95e754122bac254d8a1f4a3d21c7ae096229f8
acted <l.oad@qq.com>
2021-04-05 12:51:40 +0800

    getchar

--- test.c
+++ test.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
-typedef struct
-{
-    char name[20];
-    short price;
-}person;
-
 int main(int argc, char const *argv[])
 {
-    person p={"Êé1",20};
-    person *p2=&p;
-    printf("name=%s,price=%d\n", p.name,p.price);
-    printf("name=%s,price=%d\n", (*p2).name,(*p2).price);
-    printf("name=%s,price=%d\n", p2->name,p2->price);
+    char ch;
+    while ((ch = getchar()) != '#')
+    {
+        putchar(ch);
+    }
     return 0;
 }
