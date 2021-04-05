7a0a4d61fec29a85221af0b9505f6f04dc0aeec5
acted <l.oad@qq.com>
2021-04-05 13:57:33 +0800

    for与while的不同

--- test.c
+++ test.c
@@ -2,10 +2,26 @@
 #include <string.h>
 int main(int argc, char const *argv[])
 {
-    char ch;
-    while ((ch = getchar()) != '#')
+    for (int i = 1; i < 10; i++)
     {
-        putchar(ch);
+        //该操作只会跳过i==5之后的语句,然后执行i++;
+        if (i == 5)
+        {
+            continue;
+        }
+        printf("%d", i);
+    }
+    printf("\n");
+    int i = 0;
+    while (i <= 10)
+    {
+        //该操作会使i++;语句无法执行,导致死循环
+        if (i == 5)
+        {
+            continue;
+        }
+        printf("%d", i);
+        i++;
     }
     return 0;
 }
